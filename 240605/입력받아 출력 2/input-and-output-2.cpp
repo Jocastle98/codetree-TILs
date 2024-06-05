71097a06af03c0ae5bdbae0bd8d6ab4ca29787e6
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-06-05 16:49:59 +0900

    Title: 입력받아 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240605/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    char d;
+    cin>>a>>d>>b;
+    cout<<a<<b;
+    
+
+
+    return 0;
+}

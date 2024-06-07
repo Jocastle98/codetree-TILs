ec6770d4edf74db981baa80e17d62120447f35c4
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-06-07 16:17:25 +0900

    Title: 삼항연산자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240607/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    string b;
+    cin>>a;
+    b= a>99?"pass":"failure";
+    cout<<b;
+    return 0;
+}

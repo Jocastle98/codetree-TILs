3db96fe8d3e08d9a6d0abf8bdba92f62d5197b20
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-06-07 19:18:09 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240607/장학금/scholarship.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin>>a>>b;
+    if(a>=90){
+        if(b>=95){
+            cout<<10;
+        }
+        else if(b>=90){
+            cout<<5;
+        }
+        else{
+            cout<<0;
+        }
+    }
+    else{
+        cout<<0;
+    }
+    
+
+    return 0;
+}

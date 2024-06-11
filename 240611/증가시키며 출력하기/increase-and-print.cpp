c3d36a75f71dd98a3bd625586dcd891c29ff4f09
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-06-11 17:35:06 +0900

    Title: 증가시키며 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240611/증가시키며 출력하기/increase-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    
+    for(int i =5; i<18; i++){
+        if(i%2==1){
+            cout<<i<<" ";
+        }
+    }
+    
+    return 0;
+}

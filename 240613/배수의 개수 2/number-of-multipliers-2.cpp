d58f5bdac3a1c7c84020acbd8857ac40da3a5b13
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-06-13 15:19:37 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240613/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    int sum =0;
+    for(int i =0; i<10; i++){
+        cin>>a;
+        if(a%2==1){
+            sum++;
+        }
+
+    }
+    cout<<sum;
+    return 0;
+}

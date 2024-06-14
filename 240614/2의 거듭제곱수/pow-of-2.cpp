301bfc57c8ee616ec17a4ca8242501b4e0686261
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-06-14 18:42:36 +0900

    Title: 2의 거듭제곱수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240614/2의 거듭제곱수/pow-of-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int N;
+    cin>>N;
+    int x=1;
+    while(1){
+        x++;
+        if(N/2==x){
+            cout<<x;
+            break;
+        }
+        
+        
+
+    }
+
+    return 0;
+}

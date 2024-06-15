429fa8ea7e45bb98481c07030ecacd2661ef04a8
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-06-15 23:04:25 +0900

    Title: 모두 3의 배수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240615/모두 3의 배수/all-multiples-of-3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    bool t = false;
+
+    for(int i=1; i<=5; i++){
+        cin>>a;
+        if(a%3==0){
+            t=true;
+        }
+    }
+    if(t==true){
+        cout<<1;
+    }
+    else{
+        cout<<0;
+    }
+    
+    return 0;
+}

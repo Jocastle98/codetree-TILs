23c2902f86d74bac20ed5ad9fa59995c46d914db
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-15 14:23:48 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240815/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.cpp
+++ 240815/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.cpp
@@ -4,18 +4,20 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int  sum=0;
- 
+    int k;
     int arr[100];
 
     for(int i=0; i<100; i++){
         cin>>arr[i];
        
         if(arr[i]==0){
+            k=i;
             break;
+
         }
         sum += arr[i];
     }
 
-    cout<<sum;
+    cout<<arr[k-1]+arr[k-2]+arr[k-3];
     return 0;
 }

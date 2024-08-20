ba12d5a9ef7fbeeade0a8dc9372c5b5cdb0b75c8
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-20 15:13:12 +0900

    Title: 가장 짧은 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240820/가장 짧은 문자열/shortest-string.cpp
+++ 240820/가장 짧은 문자열/shortest-string.cpp
@@ -9,23 +9,26 @@ int main() {
     int stra = a.length();
     int strb = b.length();
     int strc = c.length();
-    if(stra> strb){
-        if(stra>strc){
-            cout<<stra-strb;
-        }
-        else{
-            cout<<strc - strb;
-        }
-    }
-    else if(strb>stra){
-        if(strb>strc){
-            cout<<strb - stra;
-        }
-        else{
-            cout<<strc- stra;
-        }
-    }
 
+    if(stra>=strb and strb>= strc){
+        cout<<stra - strc;
+    }
+    else if(stra>=strc and strc>=strb){
+        cout<<stra-strb;
+    }
+    else if(strb>=stra and stra>=strc){
+        cout<<strb - strc;
+    }
+    else if(strb>=strc and strc >=stra){
+        cout<<strb - stra;
+    }
+    else if(strc>=stra and stra>= strb){
+        cout<<strc - strb;
+    }
+    else if(strc>= strb and strb>=stra){
+        cout<<strc- stra;
+    }
+    
 
     return 0;
 }

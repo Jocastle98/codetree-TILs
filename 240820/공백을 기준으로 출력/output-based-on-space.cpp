c0d08e4a1d73bc98efdf8172df4132605aa1c0a8
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-20 16:09:58 +0900

    Title: 공백을 기준으로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240820/공백을 기준으로 출력/output-based-on-space.cpp
+++ 240820/공백을 기준으로 출력/output-based-on-space.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    string str1, str2,a;
+    string str1, str2;
 
     getline(cin,str1);
     getline(cin,str2);
 
-    for(int i=0; i<str1.length(); i++){
-        if(str1[i]==' '){
-            i++;
-        }
-        a +=str1[i];
-    }
-    for(int j=0; j<str2.length(); j++){
-        if(str2[j]==' '){
-            j++;
-        }
-        a+=str2[j];
-    }
-    cout<<a;
+    str1.erase(remove(str1.begin(), str1.end(), ' '),str1.end());
+    str2.erase(remove(str2.begin(), str2.end(),' '),str2.end());
+    cout<<str1<<str2;
     return 0;
 }

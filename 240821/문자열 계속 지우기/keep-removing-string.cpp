357d8457bae0e52b3dda945035ef1834438f5209
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-21 17:48:15 +0900

    Title: 문자열 계속 지우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240821/문자열 계속 지우기/keep-removing-string.cpp
+++ 240821/문자열 계속 지우기/keep-removing-string.cpp
@@ -6,7 +6,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     string a,b;
     cin>>a>>b;
-    for(int i=0; i<a.length(); i++){
+    while(a.length()>0){
         if(a.find(b) != string::npos){
             a.erase(a.find(b),b.length());
         }

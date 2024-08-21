42a4a9cdda7b8f2dd80a6d120860cd6f1e0b66f1
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-21 16:44:11 +0900

    Title: 첫 번째와 두 번째 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240821/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin>>str;
+    char a=str[0];
+    char b=str[1];
+   
+    for(int i=0; i<str.length(); i++){
+        if(str[i]==a){
+            str[i]=b;
+        }   
+        else if(str[i]==b){
+            str[i]=a;
+        }
+    }
+   
+    cout<<str;
+
+    return 0;
+}

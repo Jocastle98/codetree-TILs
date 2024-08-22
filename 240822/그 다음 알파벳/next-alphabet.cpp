ee6e7f09714260dce539c2abe7c650a8f3c52c05
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-22 16:08:05 +0900

    Title: 그 다음 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240822/그 다음 알파벳/next-alphabet.cpp
+++ 240822/그 다음 알파벳/next-alphabet.cpp
@@ -5,6 +5,13 @@ int main() {
     // 여기에 코드를 작성해주세요.
     char a;
     cin>>a;
-    cout<<char(int(a)+1);
+
+    if(char(int(a))=='z'){
+        cout<<'a';
+    }
+    else{
+         cout<<char(int(a)+1);
+   
+    }
     return 0;
 }

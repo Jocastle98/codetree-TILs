02332da6d48916f2e13a23f8dce27e786789170d
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-22 16:10:31 +0900

    Title: 그 전 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240822/그 전 알파벳/before-the-alphabet.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char a;
+    cin>>a;
+
+    if(char(int(a))=='a'){
+        cout<<'z';
+    }
+    else{
+        cout<<char(int(a)-1);
+    }
+    return 0;
+}

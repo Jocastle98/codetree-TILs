25a835552ce61e5f32fce673821cea0670da7e42
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-22 17:57:42 +0900

    Title: 두 수의 합과 1, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240822/두 수의 합과 1/two-nums-sum-and-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin>>a>>b;
+    int sum = a+b;
+    int cnt=0;
+    string arr = to_string(sum);
+
+    for(int i=0; i<arr.length(); i++){
+        if(arr[i]=='1'){
+            cnt++;
+        }
+    }
+    cout<<cnt;
+    
+    return 0;
+}

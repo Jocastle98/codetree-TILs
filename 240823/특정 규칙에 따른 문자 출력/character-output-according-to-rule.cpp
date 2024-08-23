378b65c3e612876a0015788b837ecfd27ff912d6
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-23 16:05:13 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240823/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        for(int j=n-i-1 ; j>0; j--){
+            cout<<"  ";
+        }
+        for(int k=0; k<i+1; k++){
+            cout<<"@ ";
+        }
+        cout<<endl;
+    }
+    for(int i=0; i<n-1; i++){
+       
+        for(int k=n-i-1; k>0; k--){
+            cout<<"@ ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}

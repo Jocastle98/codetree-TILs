aa710144eb0a196f7812e4c70bbc3a03f5ffe217
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-23 18:12:59 +0900

    Title: 순서대로 구구단, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240823/순서대로 구구단/multiplication-table-in-order.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin>>a>>b;
+
+    for(int i=1; i<=9; i++){
+        cout<<a<<" * "<<i<<" = "<<a*i<<"  "<<b<<" * "<<i<<" = "<<i*b<<endl;
+    }
+    return 0;
+}

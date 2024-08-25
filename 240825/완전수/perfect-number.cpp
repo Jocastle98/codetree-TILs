a9c8922e1f7ef739251a6be7a688618dafbc8554
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-25 17:13:32 +0900

    Title: 완전수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240825/완전수/perfect-number.cpp
+++ 240825/완전수/perfect-number.cpp
@@ -7,26 +7,25 @@ int main() {
     cin>>a>>b;
     
     int cnt=0;
-    int sum=0;
+    
     for(int i=a; i<=b; i++){
+        int sum=0;
         for(int j=1; j<i; j++){
-            if(i%j==0 and j!=i){
+            if(i%j==0 and i!=j){
                 sum +=j; 
-
-                if(sum==i){
-                    //cout<<sum<<endl;;
-                    cnt++;
-                }
+                
             }
-            
         }
+        if(sum==i){
+                //cout<<sum<<endl;;
+        cnt++;
+        //cout<<sum<<endl;
+        }
+            
+            
+        
     }
-    if(cnt>0){
-    cout<<cnt-1;
-    }
-    else{
-        cout<<0;
-    }
+    cout<<cnt;
 
     return 0;
 }

f94cb9612e539eb1f780217ce24ddc6734ea20fa
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-25 16:46:18 +0900

    Title: 완전수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240825/완전수/perfect-number.cpp
+++ 240825/완전수/perfect-number.cpp
@@ -5,21 +5,28 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
     cin>>a>>b;
+    
     int cnt=0;
     int sum=0;
     for(int i=a; i<=b; i++){
         for(int j=1; j<i; j++){
-            if(i%j==0){
-                sum +=j;
-                
+            if(i%j==0 and j!=i){
+                sum +=j; 
+
                 if(sum==i){
+                    //cout<<sum<<endl;;
                     cnt++;
                 }
             }
             
         }
     }
+    if(cnt>0){
     cout<<cnt-1;
+    }
+    else{
+        cout<<0;
+    }
 
     return 0;
 }

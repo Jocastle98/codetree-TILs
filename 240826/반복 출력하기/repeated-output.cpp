25e47973db92bd349947f93eeb305c6525c020bd
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-26 17:26:03 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240826/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void pr(int n){
+    for(int i=0; i<n; i++){
+        cout<<"12345^&*()_"<<endl;
+    }
+    
+    
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요
+    int a;
+    cin>>a;
+
+    pr(a);
+
+    return 0;
+}

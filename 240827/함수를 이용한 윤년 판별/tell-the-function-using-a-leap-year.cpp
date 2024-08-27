dba51eae9576c1f74f295ccca972659e9a9780bc
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-27 17:17:04 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240827/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+bool year(int n){
+    if(n%4!=0) return false;
+    if(n%100==0 and n%400 !=0)return false;
+
+    return true;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int y;
+    cin>>y;
+
+    if(year(y)) cout<<"true";
+    else cout<<"false";
+    return 0;
+}

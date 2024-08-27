ca1e942c60867526db78ac5a10977a9124046b58
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-27 16:49:19 +0900

    Title: 함수를 이용한 369 게임, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240827/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 240827/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 using namespace std;
 
-void cnt3(int a,int b){
-    int cnt =0;
-    for(int i=a; i<=b; i++){
-        if(i%10 ==3 or i%10 ==6 or i%10==9 or i/10 ==3 or i/10==6 or i/10==9 or i%3==0){
-            cnt++;
-        }
-    }
-    cout<<cnt;
-}
+// void cnt3(int a,int b){
+//     int cnt =0;
+//     for(int i=a; i<=b; i++){
+//         if(i%10 ==3 or i%10 ==6 or i%10==9 or i/10 ==3 or i/10==6 or i/10==9 or i%3==0){
+//             cnt++;
+//         }
+//     }
+//     cout<<cnt;
+// }
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int cnt =0;
     int a,b;
     cin>>a>>b;
-    cnt3(a,b);
+    
+    for(int i=a; i<=b; i++){
+        if(i%10 ==3 or i%10 ==6 or i%10==9 or i/10 ==3 or i/10==6 or i/10==9 or i%3==0){
+            cnt++;
+        }
+    }
+    cout<<cnt;
     return 0;
 }

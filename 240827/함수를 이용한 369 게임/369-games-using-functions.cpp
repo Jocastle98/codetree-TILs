4b1c116962b999b996267158dddbc6122e96a661
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-27 17:02:06 +0900

    Title: 함수를 이용한 369 게임, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240827/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 240827/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 using namespace std;
 
-// void cnt3(int a,int b){
-//     int cnt =0;
-//     for(int i=a; i<=b; i++){
-//         if(i%10 ==3 or i%10 ==6 or i%10==9 or i/10 ==3 or i/10==6 or i/10==9 or i%3==0){
-//             cnt++;
-//         }
-//     }
-//     cout<<cnt;
-// }
+// 숫자에 3, 6, 9가 들어가 있거나 3의 배수인 숫자의 개수를 세는 함수
+int cnt3(int a, int b) {
+    int cnt = 0;
+    for (int i = a; i <= b; i++) {
+        int num = i;
 
-int main() {
-    // 여기에 코드를 작성해주세요.
-    int cnt =0;
-    int a,b;
-    cin>>a>>b;
-    
-    for(int i=a; i<=b; i++){
-        if(i%10 ==3 or i%10 ==6 or i%10==9 or i/10 ==3 or i/10==6 or i/10==9 or i%3==0){
+        // 숫자에 3, 6, 9가 포함되어 있는지 확인
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit == 3 || digit == 6 || digit == 9) {
+                cnt++;
+                break; // 3, 6, 9가 하나라도 포함되어 있으면 다음 숫자로 넘어감
+            }
+            num /= 10;
+        }
+
+        // 만약 위의 루프에서 cnt가 증가하지 않았고, 숫자가 3의 배수이면 cnt 증가
+        if (num == 0 && i % 3 == 0) {
             cnt++;
         }
     }
-    cout<<cnt;
+    return cnt;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    // cnt3 함수를 호출하여 결과를 출력
+    int result = cnt3(a, b);
+    cout << result << endl;
+
     return 0;
 }

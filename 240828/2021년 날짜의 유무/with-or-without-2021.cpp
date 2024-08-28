3bf941f984b24ab135d2011aa7df61d7c14cd71e
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-08-28 17:42:44 +0900

    Title: 2021년 날짜의 유무, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240828/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+bool year(int m, int d){
+    if(m>=8 and m<=12){
+        if(m%2==0 and d<=31){
+            return true;
+        }
+        else if(m%2==1 and d<=30){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    if(m<8){
+        if(m%2==1 and d<=31){
+            return true;
+        }
+        else if(m%2==0 and d<=30){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    return false;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m,d;
+    cin>>m>>d;
+
+    if(year(m,d)){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+
+    return 0;
+}

590809ce8ab24628800fec83851bc70436142dfa
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-01 16:01:07 +0900

    Title: 절댓값으로 변경, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240901/절댓값으로 변경/find-the-absolute-value.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void abso(int arr[], int &n){
+    for(int i=0; i<n; i++){
+        if(arr[i]<0){
+            arr[i] = arr[i]* (-1);
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    abso(arr,n);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}

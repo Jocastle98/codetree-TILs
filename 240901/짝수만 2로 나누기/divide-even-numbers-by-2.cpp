d900be1373201ad39c9a0436a280cf37a11263e7
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-01 15:43:18 +0900

    Title: 짝수만 2로 나누기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240901/짝수만 2로 나누기/divide-even-numbers-by-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void chan(int arr[],int n){
+    for(int i=0; i<n; i++){
+        if(arr[i]%2==0){
+            arr[i] /=2;
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    chan(arr,n);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}

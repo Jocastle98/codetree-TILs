56aa93d0bc6940785ffeadc09a5d7fb7793e85b0
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-02 15:43:21 +0900

    Title: 숫자 차례로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240902/숫자 차례로 출력하기/to-output-numerically.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void pri(int n){
+    if(n==0) return;
+    for(int i=1; i<=n; i++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    // pri(n-1);
+    for(int i=n; i>=1; i--){
+        cout<<i<<" ";
+    }
+
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+
+    pri(n);
+    return 0;
+}

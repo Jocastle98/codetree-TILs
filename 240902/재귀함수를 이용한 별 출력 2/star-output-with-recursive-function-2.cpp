f6727a2f3cd4f143aad17c39b73e17a3dd5944b3
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-02 15:56:23 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240902/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void pri(int n){
+    if(n==0) return;
+
+    for(int i=n; i>=1; i--){
+        cout<<"* ";
+    }
+    cout<<endl;
+    pri(n-1);
+    for(int i=0; i<n; i++){
+        cout<<"* ";
+    }
+    cout<<endl;
+
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    pri(n)
+;    return 0;
+}

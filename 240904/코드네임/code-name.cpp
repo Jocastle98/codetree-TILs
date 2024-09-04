573723da964481b316ca274a658e92829af72b2f
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-04 16:35:37 +0900

    Title: 코드네임, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240904/코드네임/code-name.cpp
+++ 240904/코드네임/code-name.cpp
@@ -2,35 +2,40 @@
 #include <string>
 using namespace std;
 
-class Spy{
-    public:
-        string name;
-        int score;
-    
-        Spy(string name="", int score=0){
-            this->name =name;
-            this->score =score;
-        }
+class Spy {
+public:
+    string name;
+    int score;
+
+    // 생성자
+    Spy(string name = "", int score = 0) {
+        this->name = name;
+        this->score = score;
+    }
 };
 
-
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 스파이 배열 5명
     Spy spys[5];
-    
-    int k=0;
-    for(int i=0; i<5; i++){
+
+    // 입력 받기
+    for (int i = 0; i < 5; i++) {
         string name;
         int score;
-        cin>>name>>score;
-        spys[i] = Spy(name,score);
-        int min=spys[0].score;
-        if(spys[i].score<min){
-            k=i;
-        }
-
+        cin >> name >> score;
+        spys[i] = Spy(name, score);  // 객체 생성 및 배열에 저장
     }
-    cout<<spys[k].name<<" "<<spys[k].score;
+
+    // 최소 점수를 가진 요원 찾기
+    int minIndex = 0;
+    for (int i = 1; i < 5; i++) {
+        if (spys[i].score < spys[minIndex].score) {
+            minIndex = i;
+        }
+    }
+
+    // 최소 점수 요원 정보 출력
+    cout << spys[minIndex].name << " " << spys[minIndex].score << endl;
 
     return 0;
 }

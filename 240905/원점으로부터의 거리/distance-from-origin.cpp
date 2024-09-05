bcf4223967165a080a59d3fc764e61dd19e61bb7
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-05 17:21:29 +0900

    Title: 원점으로부터의 거리, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240905/원점으로부터의 거리/distance-from-origin.cpp
+++ 240905/원점으로부터의 거리/distance-from-origin.cpp
@@ -12,10 +12,10 @@ class ap{
         }
 };
 bool cmp(ap a, ap b){
-    if(a.x+a.y==b.x+b.y){
+    if(a.x*a.x+a.y*a.y==b.x*b.x+b.y*b.y){
         return a.n<b.n;
     }
-    return a.x+a.y<b.x+b.y;
+    return a.x*a.x+a.y*a.y<b.x*b.x+b.y*b.y;
 }
 
 int main() {

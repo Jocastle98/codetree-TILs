144c6e38eac97d86e310c73ab428cdf8d2a162ab
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-06 15:34:06 +0900

    Title: Date to Date, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240906/Date to Date/date-to-date.cpp
+++ 240906/Date to Date/date-to-date.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int mon(int a) {
+    if (a == 2) return 28; // 2월은 28일 (윤년이 아니므로)
+    
+    if (a == 4 || a == 6 || a == 9 || a == 11) 
+        return 30; // 4, 6, 9, 11월은 30일
+    
+    return 31; // 나머지 달은 31일
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
     int a,b,c,d;
     cin>>a>>b>>c>>d;
     int day =1;
-    int num[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    
     while(true){
         if(a==c and b==d){
             break;
         }
         day++;
         b++;
-        if(b>num[a]){
+        if(b>mon(a)){
             a++;
             b=1;
         }

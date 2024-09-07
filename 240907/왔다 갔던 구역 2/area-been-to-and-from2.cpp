bb193535e0a36c57117b7e7bdf455bcf2edfd00c
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-07 17:53:53 +0900

    Title: 왔다 갔던 구역 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240907/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240907/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -2,37 +2,46 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     int n;
-    cin>>n;
-    int x;
-    int a=0;
-    int cur =50000;
-    char dir;
-    int arr[100000]={0};
-    for(int i=0; i<n; i++){
-        cin>>x>>dir;
-        if(dir=='L'){
-            for(int j=cur-1; j>=cur-x; j--){
-                arr[j]++;
-            }
-            cur -=x;
-        }
+    cin >> n;
 
-        else{
-            for(int k=cur+1; k<=cur+x; k++){
-                arr[k]++;
+    const int OFFSET = 5000;   // 음수 좌표를 양수로 변환하기 위한 오프셋
+    const int MAX = 10000;     // 좌우로 충분히 이동할 수 있는 범위
+    int arr[MAX] = {0};        // 0으로 초기화된 배열 (이동 경로 기록용)
+
+    int position = OFFSET;     // 시작 위치를 0으로 설정 (OFFSET 적용)
+    int x;
+    char dir;
+
+    // n개의 명령 입력
+    for (int i = 0; i < n; i++) {
+        cin >> x >> dir;
+
+        if (dir == 'L') {
+            // 왼쪽으로 이동: 현재 위치에서 왼쪽으로 x만큼 이동
+            for (int j = position - 1; j >= position - x; j--) {
+                arr[j]++;  // 지나간 흔적 기록
             }
-            cur +=x;
+            position -= x;  // 새로운 위치 갱신
+        } else if (dir == 'R') {
+            // 오른쪽으로 이동: 현재 위치에서 오른쪽으로 x만큼 이동
+            for (int j = position + 1; j <= position + x; j++) {
+                arr[j]++;  // 지나간 흔적 기록
+            }
+            position += x;  // 새로운 위치 갱신
         }
-        
     }
-    int max =0;
-    for(int i=0; i<=100000; i++){
-        if(arr[i]>1){
-            max++;
+
+    // 2번 이상 지나간 영역 크기 계산
+    int result = 0;
+    for (int i = 0; i < MAX; i++) {
+        if (arr[i] > 1) {
+            result++;
         }
     }
-    cout<<max;
+
+    // 결과 출력
+    cout << result << endl;
+
     return 0;
 }

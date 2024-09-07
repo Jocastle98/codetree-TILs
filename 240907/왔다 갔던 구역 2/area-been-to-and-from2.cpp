a038a4f96f2707273b4c455b2730f20d687e0604
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-07 17:47:57 +0900

    Title: 왔다 갔던 구역 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240907/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int x;
+    int a=0;
+    int cur =10000;
+    char dir;
+    int arr[20000]={0};
+    for(int i=0; i<n; i++){
+        cin>>x>>dir;
+        if(dir=='L'){
+            for(int j=cur; j>cur-x; j--){
+                arr[j]++;
+            }
+            cur -=x;
+        }
+
+        else{
+            for(int k=cur; k<cur+x; k++){
+                arr[k]++;
+            }
+            cur +=x;
+        }
+        
+    }
+    int max =0;
+    for(int i=0; i<=20000; i++){
+        if(arr[i]>1){
+            max++;
+        }
+    }
+    cout<<max;
+    return 0;
+}

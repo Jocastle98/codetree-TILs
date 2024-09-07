bc7791d3658db8e2ff2a274cef3ab55470dfd6a2
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-07 17:01:30 +0900

    Title: 최대로 겹치는 구간, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240907/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 240907/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -5,20 +5,24 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin>>n;
-    int arr[n] ={};
+     
+    int arr[1000] = {0}; 
 
     for(int i=0; i<n; i++){
         int a,b;
         cin>>a>>b;
-        for(int j=a-1; j<=b-1; j++){
-            arr[j] +=1;
-        }
+        // 선분이 시작하는 지점에서 +1
+        arr[a]++;
+        // 선분이 끝나는 지점에서 겹치지 않으므로 b에서 -1이 아니라 b-1에서 처리
+        arr[b]--;
 
     }
-    int max =arr[0];
-    for(int i=1; i<n; i++){
-        if(arr[i]>max){
-            max =arr[i];
+    int max =0;
+    int cur =0;
+    for(int i=0; i<n; i++){
+        cur +=arr[i];
+        if(cur>max){
+            max =cur;
         }
     }
     cout<<max;

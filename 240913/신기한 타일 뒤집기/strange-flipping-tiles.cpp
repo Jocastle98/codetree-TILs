3b6ab12d384ed1510907cc18e3003601b1ed8d61
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-13 16:29:43 +0900

    Title: 신기한 타일 뒤집기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240913/신기한 타일 뒤집기/strange-flipping-tiles.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+
+#define max_x 2000
+#define off 1000
+
+int n;
+
+int arr[2*max_x+1];
+int black[2*max_x+1],white[2*max_x+1];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n;
+
+    int cur =1000;
+    int b=0,w=0;
+    for(int i=0; i<n; i++){
+        int dis;
+        char dir;
+        cin>>dis>>dir;
+        if(dir =='R'){
+            while(dis--){
+                arr[cur]=1;
+                black[cur]++;
+                if(dis)cur++;
+            }
+        }
+        else{
+            while(dis--){
+            arr[cur] =2;
+            white[cur]++;
+            if(dis)cur--;
+            }
+        }
+        
+    }
+    for(int i=0; i<=2*max_x; i++){
+        if(arr[i]==1) b++;
+        if(arr[i]==2) w++;
+    }
+    cout<<w<<" "<<b;
+    
+    return 0;
+}

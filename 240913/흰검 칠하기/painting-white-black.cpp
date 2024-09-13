2157e90818a3e1f08a015c362fa4f4956f2d0c72
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-13 16:11:14 +0900

    Title: 흰검 칠하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240913/흰검 칠하기/painting-white-black.cpp
+++ 240913/흰검 칠하기/painting-white-black.cpp
@@ -1,64 +1,52 @@
 #include <iostream>
+
 using namespace std;
 
-#define MAX_SIZE 20001  // 충분히 큰 배열 크기 (좌우 10000칸씩 여유분)
-#define OFFSET 10000    // 중앙 기준 오프셋 설정
+#define MAX_K 100000
+
+int n;
+int a[2 * MAX_K + 1];
+int cnt_b[2 * MAX_K + 1];
+int cnt_w[2 * MAX_K + 1];
+int b, w, g;
 
 int main() {
-    int n;
+    // 변수 입력
     cin >> n;
 
-    int white_count[MAX_SIZE] = {0};  // 타일이 흰색으로 칠해진 횟수
-    int black_count[MAX_SIZE] = {0};  // 타일이 검은색으로 칠해진 횟수
-    bool is_gray[MAX_SIZE] = {false}; // 타일이 회색이 되었는지 여부
-    int cur = OFFSET;  // 시작 위치는 배열 중앙
+    int cur = MAX_K;
 
-    for (int i = 0; i < n; ++i) {
-        int dis;
-        char dir;
-        cin >> dis >> dir;
-
-        if (dir == 'L') {
-            // 현재 위치도 포함해서 왼쪽으로 이동하며 타일을 흰색으로 칠함
-            for (int j = 0; j < dis; j++) {
-                if (!is_gray[cur]) {  // 타일이 회색이 아닌 경우에만
-                    white_count[cur]++;  // 흰색으로 덧칠
-                    // 회색 조건 확인
-                    if (white_count[cur] >= 2 && black_count[cur] >= 2) {
-                        is_gray[cur] = true;
-                    }
-                }
-                cur--;  // 왼쪽으로 한 칸 이동
+    for(int i = 1; i <= n; i++) {
+        int x;
+        char c;
+        cin >> x >> c;
+        if(c == 'L') {
+            // x칸 왼쪽으로 칠합니다.
+            while(x--) {
+                a[cur] = 1;
+                cnt_w[cur]++;
+                if(x) cur--;
             }
-        } else if (dir == 'R') {
-            // 현재 위치도 포함해서 오른쪽으로 이동하며 타일을 검은색으로 칠함
-            for (int j = 0; j < dis; j++) {
-                if (!is_gray[cur]) {  // 타일이 회색이 아닌 경우에만
-                    black_count[cur]++;  // 검은색으로 덧칠
-                    // 회색 조건 확인
-                    if (white_count[cur] >= 2 && black_count[cur] >= 2) {
-                        is_gray[cur] = true;
-                    }
-                }
-                cur++;  // 오른쪽으로 한 칸 이동
+        }
+        else {
+            // x칸 오른쪽으로 칠합니다.
+            while(x--) {
+                a[cur] = 2;
+                cnt_b[cur]++;
+                if(x) cur++;
             }
         }
     }
 
-    int white = 0, black = 0, gray = 0;
-
-    // 배열 순회하여 흰색, 검은색, 회색 타일 카운트
-    for (int i = 0; i < MAX_SIZE; ++i) {
-        if (is_gray[i]) {
-            gray++;  // 회색
-        } else if (white_count[i] > 0 && black_count[i] == 0) {
-            white++;  // 흰색
-        } else if (black_count[i] > 0 && white_count[i] == 0) {
-            black++;  // 검은색
-        }
+    for(int i = 0; i <= 2 * MAX_K; i++) {
+        // 검은색과 흰색으로 두 번 이상 칠해진 타일은 회색입니다.
+        if(cnt_b[i] >= 2 && cnt_w[i] >= 2) g++;
+        // 그렇지 않으면 현재 칠해진 색깔이 곧 타일의 색깔입니다.
+        else if(a[i] == 1) w++;
+        else if(a[i] == 2) b++;
     }
 
-    cout << white << " " << black << " " << gray << endl;
-
+    // 정답을 출력합니다.
+    cout << w << " " << b << " " << g;
     return 0;
 }

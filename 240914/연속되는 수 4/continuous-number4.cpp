4bb2dd912e46769a24e784fd6b4ca172b9f01a3d
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-14 16:11:18 +0900

    Title: 연속되는 수 4, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240914/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int arr[1000];
+    int cnt =0, ans =0;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    for(int i=1; i<n; i++){
+        if(i>=1 and (arr[i]>arr[i-1])){
+            cnt++;
+        }
+        else{
+            cnt =1;
+        }
+        ans = max(ans,cnt);
+    }
+    cout<<ans;
+
+    return 0;
+}

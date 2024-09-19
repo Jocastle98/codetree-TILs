d517078835290b50f7e822325bed8708b65dd619
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-19 16:15:45 +0900

    Title: 문자에 따른 명령 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240919/문자에 따른 명령 2/text-based-commands2.cpp
+++ 240919/문자에 따른 명령 2/text-based-commands2.cpp
@@ -1,38 +1,27 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    int x=0,y=0;
-    int dx[4]={};
-    int dy[4]={};
-    string a;
-    cin>>a;
-    if(a.find('L') != string::npos){
-        if(a.find('F')!=string::npos){
-            x -=1;
-            y;
-        }
-        else{
-            x;
-            y;
+    int x = 0, y = 0; // 현재 위치
+    int dir = 0;      // 현재 바라보는 방향 (0: 북, 1: 동, 2: 남, 3: 서)
+    int dx[4] = {0, 1, 0, -1}; // 각 방향에 따른 x 변화량
+    int dy[4] = {1, 0, -1, 0}; // 각 방향에 따른 y 변화량
 
+    string commands;
+    cin >> commands; // 명령어 문자열 입력받기
+
+    for (char command : commands) {
+        if (command == 'L') {
+            dir = (dir + 3) % 4; // 왼쪽으로 90도 회전
+        } else if (command == 'R') {
+            dir = (dir + 1) % 4; // 오른쪽으로 90도 회전
+        } else if (command == 'F') {
+            x += dx[dir];
+            y += dy[dir];
         }
     }
-    else if(a.find('R') !=string::npos){
-        if(a.find('F')!=string::npos){
-            x+=1;
-            y;
 
-        }
-        else{
-            x;
-            y;
-
-        }
-    }
-    cout<<x<<" "<<y;
+    cout << x << " " << y << endl;
     return 0;
 }

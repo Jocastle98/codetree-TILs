7d6015b4baca70c8c803d7ef458c9cefaf4e1deb
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-20 17:37:41 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240920/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int n;
+bool InRange(int x,int y){
+    return (0<=x and x<n and 0<=y and y<n);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n;
+    int arr[100][100]={0};
+    int dx[4] ={0,-1,0,1};//우상좌하
+    int dy[4] ={1,0,-1,0};
+    int dir =0;
+    int x= toupper(n/2), y=toupper(n/2);
+    for(int cnt =1; cnt<=n*n; cnt++){
+        int nx= x+dx[dir];
+        int ny = y+dy[dir];
+        arr[x][y] = cnt;
+        if(InRange(nx,ny) and arr[nx][ny]==0){
+            x=nx;
+            y=ny;
+        }
+        else{
+            dir = (dir+1)%4;
+            x+=dx[dir];
+            y+=dy[dir];
+        }
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}

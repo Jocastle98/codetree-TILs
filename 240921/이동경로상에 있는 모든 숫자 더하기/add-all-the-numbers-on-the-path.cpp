f7519889bb41c6d1cfb91f6f3eac281212e841a0
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-21 16:22:52 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240921/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int n;
+bool InRange(int x,int y){
+    return (0<=x and x<n and 0<=y and y<n);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int t;
+    cin>>n>>t;
+    int x=n/2 ,y=n/2;
+    int dx[4] ={-1,0,1,0};//북동남서
+    int dy[4] ={0,1,0,-1};
+    int dir= 0;
+    string commands;
+    cin>>commands;
+    int arr[101][101] ={0};
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin>>arr[i][j];
+            //cout<<arr[i][j]<<" ";
+        }
+        //cout<<endl;
+    }
+    int sum=arr[x][y];
+    for(char command: commands){
+        
+        if(command=='L'){
+            dir = (dir+3)%4;
+        }
+        else if(command=='R'){
+            dir = (dir+1)%4;
+        }
+        else{
+            if(InRange(x,y)){
+                sum+=arr[x][y];
+                x+=dx[dir];
+                y+=dy[dir];
+            }
+            else{
+                x+=dx[dir];
+                y+=dy[dir];
+            }
+        }
+    }
+    cout<<sum;
+    return 0;
+}

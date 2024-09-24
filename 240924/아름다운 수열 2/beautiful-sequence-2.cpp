0e511a275da5581524693e88eb67f6224fcfa2db
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-24 17:03:06 +0900

    Title: 아름다운 수열 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240924/아름다운 수열 2/beautiful-sequence-2.cpp
+++ 240924/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
-#include <vector>
-using namespace std;
-int main() {
-    int n, m;
-    cin >> n >> m;
+#include <algorithm>
 
-    vector<int> arr(n);
-    vector<int> beu;
+#define MAX_N 100
+
+using namespace std;
+
+int n, m;
+int arr1[MAX_N], arr2[MAX_N];
+int tmp[MAX_N];
+
+int main() {
+    // 입력
+    cin >> n >> m;
     for(int i = 0; i < n; i++)
-        cin >> arr[i];
-    for(int i = 0; i < m; i++){
-        int v;
-        cin >> v;
-        beu.push_back(v);
-    }
-        
+        cin >> arr1[i];
+	for(int i = 0; i < m; i++)
+        cin >> arr2[i];
+	
+	sort(arr2, arr2 + m);
+	
+    // 모든 구간의 시작점을 잡아봅니다.
     int cnt = 0;
-    for(int i = 0; i < n - m + 1; i++){
-        vector<int> v = beu;
-        for(int j = 0; j < m; j++){
-            for(int k = 0; k < v.size(); k++)
-                if(v[k] == arr[i + j]){
-                    swap(v[k], v.back());
-                    v.pop_back();
-                    break;
-                }
-        } 
-        if(v.size() == 0)
-            cnt++;
+    for(int i = 0; i <= n - m; i++) {
+        for(int j = 0; j < m; j++)
+            tmp[j] = arr1[i + j];
+		sort(tmp, tmp + m);
+		
+		bool issame = true;
+		for(int j = 0; j < m; j++)
+			if(tmp[j] != arr2[j]) {
+				issame = false;
+				break;
+			}
+		
+		if(issame)
+			cnt++;
     }
+                        
     cout << cnt;
     return 0;
 }

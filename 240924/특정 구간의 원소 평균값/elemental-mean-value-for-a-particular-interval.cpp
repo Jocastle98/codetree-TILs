5cd99580c2cff42396bee3d8c535c8fdad0155d0
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-24 16:48:00 +0900

    Title: 특정 구간의 원소 평균값, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240924/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
+++ 240924/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     int n;
-    cin>>n;
-    int arr[100]={0};
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+    cin >> n;
+    int arr[100] = {0};
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
     }
-    int cnt =0; 
-    for(int i=0; i<n; i++){
-        int sum= 0;
+    int cnt = 0; 
+    for(int i = 0; i < n; i++){
+        int sum = 0;
+        // 구간의 합 구하기
         for(int j = i; j < n; j++){
             sum += arr[j];
             int len = j - i + 1; // 구간의 길이
-            int avg = sum / len; // 평균 계산
+            double avg = (double)sum / len; // 평균 계산
             // 구간 내 원소 중 평균과 같은 값이 있는지 확인
             for(int k = i; k <= j; k++){
                 if(arr[k] == avg){
@@ -24,8 +24,7 @@ int main() {
                 }
             }
         }
-
     }
-    cout<<cnt-n;
+    cout << cnt << endl;
     return 0;
 }

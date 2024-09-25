48680696f5fb20468fe06b07b539aba154596bab
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-25 17:17:39 +0900

    Title: 개발자의 능력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240925/개발자의 능력 2/ability-of-developer-2.cpp
+++ 240925/개발자의 능력 2/ability-of-developer-2.cpp
@@ -10,15 +10,35 @@ int arr[6]={0};
 
 int main() {
     // 여기에 코드를 작성해주세요.
+    int total =0;
+    for(int i=0; i<6; i++){
+        cin>>arr[i];
+        total+=arr[i];
+    }
+
+    int ans =INT_MAX;
     
-    for(int i=0; i<6; i++) cin>>arr[i]; 
-    sort(arr,arr+6); //숫자를 정렬
-    int a=arr[0]+arr[5];
-    int b= arr[1]+arr[4];
-    int c=arr[2]+arr[3];
-    int big = max(a,max(b,c));
-    int small = min(a,min(b,c));
-    cout<<big-small;
-    
+    //배정을 위한 반복문
+    for(int i=0; i<=5; i++){
+        int dif;
+        int sum3 =0;
+        for(int j=i+1; j<=5; j++){
+            for(int k=0; k<=5; k++ ){
+                for(int l=k+1; l<=5; l++){
+                    if(k == i || k == j || l == i || l == j)
+						continue;
+                    int sum1 = arr[i]+arr[j];
+                    int sum2 = arr[k]+arr[l];
+                    sum3 = total - (sum1+sum2);
+                    int big = max(sum1,max(sum2,sum3));
+                    int small = min(sum1,min(sum2,sum3));
+                    dif = big-small;
+                    ans = min(ans,dif);
+                    
+                }
+            }
+        }
+    }
+    cout<<ans;
     return 0;
 }

70a111d0f19eff2dfe5be084f68c94d65400ea1f
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2024-09-25 15:54:03 +0900

    Title: 개발자의 능력 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240925/개발자의 능력 3/ability-of-developer-3.cpp
+++ 240925/개발자의 능력 3/ability-of-developer-3.cpp
@@ -3,29 +3,29 @@
 #include <climits>
 #include <cmath>
 using namespace std;
+
 int arr[6]={0};
-int dif(int i, int j, int k){
-    int sum1 = arr[i]+arr[j]+ arr[k];
-    int sum2=0;
-    for(int i=0; i<6; i++){
-        sum2+=arr[i];
-    }
-    sum2 -=sum1;
-    return abs(sum1-sum2);
-}
+
 
 int main() {
     // 여기에 코드를 작성해주세요.
- 
+    int sum=0;
     for(int i=0; i<6; i++){
         cin>>arr[i]; //사람들의 능력
+        sum+=arr[i];
     }
     int ans =INT_MAX;
     
     for(int i=0; i<6; i++){
+        int dif;
+        int sum1=0;
+        
         for(int j=i+1; j<6; j++){
             for(int k=j+1; k<6; k++){
-                ans = min(ans,dif(i,j,k));
+                int sum2 = arr[i]+arr[j]+arr[k];
+                sum1 = sum-sum2;
+                dif = abs(sum1-sum2);
+                ans = min(ans , dif);
             }
         }
         

6f38bf16f0f075851b0ba5814870cf11627d77b1
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2025-01-27 16:14:01 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250127/중복되지 않는 정수 중 최대/max-of-unique-number.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+    
+
+    // Write your code here!
+    int n;
+    cin>>n;
+    int arr[n] ={};
+    for(int i=0; i<n; i++){
+        int a;
+        cin>>a;
+        arr[a]++;
+    }
+
+    int max_num = -1;
+    for(int i=0; i<n; i++){
+        if(arr[i] ==1 && i>=max_num){
+            max_num = i;
+        }
+    }
+    cout<<max_num;
+
+
+    return 0;
+}

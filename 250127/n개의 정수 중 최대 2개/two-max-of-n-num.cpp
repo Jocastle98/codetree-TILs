635c225a0b485bbc425a56d935235321802184f8
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2025-01-27 16:01:23 +0900

    Title: n개의 정수 중 최대 2개, Time: 18ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250127/n개의 정수 중 최대 2개/two-max-of-n-num.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+    // Write your code here!
+    int n;
+    cin>>n;
+    vector<int> v;
+    for(int i=0; i<n; i++){
+        int a;
+        cin>>a;
+        v.push_back(a);
+    }
+    sort(v.begin(), v.end());
+    for(int i =v.size()-1; i>=v.size()-2; i--){
+        cout<<v[i]<<" ";
+    }
+
+    return 0;
+}

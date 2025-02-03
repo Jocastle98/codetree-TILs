2e4a26755fe736a2085b257af40ac43910acd224
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2025-02-03 16:55:35 +0900

    Title: 특정 원소들의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250203/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int sum =0;
+    vector<vector<int>> v(4, vector<int>(4));
+
+    for(int i=0; i<4; i++){
+        for(int j= 0; j<4; j++){
+            cin>>v[i][j];
+        }
+    }
+
+    for(int i=0; i<4; i++){
+        for(int j=0; j<=i; j++){
+            sum += v[i][j];
+        }
+    }
+    cout<<sum;
+    return 0;
+}

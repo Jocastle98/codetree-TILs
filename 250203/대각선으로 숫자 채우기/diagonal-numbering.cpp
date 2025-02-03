79897c8321c8094602146f2e1731502bdce0869c
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2025-02-03 17:53:02 +0900

    Title: 대각선으로 숫자 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250203/대각선으로 숫자 채우기/diagonal-numbering.cpp
+++ 250203/대각선으로 숫자 채우기/diagonal-numbering.cpp
@@ -10,19 +10,19 @@ int main() {
     vector<vector<int>> v(n, vector<int>(m, 0));
     int num = 1;
 
-    // 대각선 방향으로 숫자 채우기
+
     for (int d = 0; d < n + m - 1; d++) {
-        int row = (d < n) ? d : n - 1;
-        int col = (d < n) ? 0 : d - (n - 1);
-        
-        while (row >= 0 && col < m) {
+        int row = (d < m) ? 0 : d - (m - 1);
+        int col = (d < m) ? d : m - 1;
+
+        while (row < n && col >= 0) {
             v[row][col] = num++;
-            row--;
-            col++;
+            row++;
+            col--;
         }
     }
 
-    // 행렬 출력
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cout << v[i][j] << " ";

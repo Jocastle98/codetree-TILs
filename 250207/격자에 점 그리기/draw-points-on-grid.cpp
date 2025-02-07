ef160de8cf29b6bbc8ac03b4192267dc5e3b2fcd
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2025-02-07 16:33:53 +0900

    Title: 격자에 점 그리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250207/격자에 점 그리기/draw-points-on-grid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>v(n,vector<int>(n));
+
+    for(int i=0; i<m; i++){
+        int r,c;
+        cin>>r>>c;
+        v[r-1][c-1] = i+1;
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
